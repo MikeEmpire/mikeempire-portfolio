70fb6882f8f9a4c151af79e9861bdcd7fc790b57
Michael Olie <aolie94@gmail.com>
2024-06-01 21:17:04 -0700

    updated intro animation to work as expected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const IndexPage: React.FC<PageProps> = () => {
   };
 
   React.useEffect(() => {
-    const timer = setTimeout(() => setShowContent(true), 2000); // Show the rest of the content after 2 seconds
+    const timer = setTimeout(() => setShowContent(true), 1800); // Show the rest of the content after 2 seconds
     return () => clearTimeout(timer);
   }, []);
 
@@ -64,67 +64,70 @@ const IndexPage: React.FC<PageProps> = () => {
     <Layout>
       <main style={pageStyles}>
         <AnimatePresence>
-          {!showContent ? (
-            <motion.h1
-              className="text-4xl text-center"
-              initial={{ opacity: 0, y: -100 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 100 }}
-              transition={{ duration: 1.5 }}
-            >
-              Welcome To Mike Empire's Portfolio
-            </motion.h1>
-          ) : (
-            <motion.div
-              className="relative w-full h-full"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1.5, duration: 1.5 }}
-            >
-              <picture>
-                <img
-                  src={bgGif}
-                  alt="Background"
-                  className="w-full h-full object-cover absolute"
-                  style={{ zIndex: 1 }}
-                />
-              </picture>
-              <div className="flex flex-row max-600:flex-col md:h-screen max-600:h-screen">
-                <div
-                  className="relative p-3 w-28"
-                  style={{
-                    zIndex: 2,
-                  }}
-                >
-                  <section className="mb-8 max-600:flex max-600:gap-5 max-600:items-center">
-                    <h1 className="text-3xl">Mike Empire</h1>
-                    <h2 className="text-xl">Software Engineer</h2>
-                  </section>
-                  <ul>{tabOptions}</ul>
-                </div>
-                <div
-                  className="max-h-full relative w-full pr-8 max-600:p-0"
-                  style={{
-                    zIndex: 2,
-                  }}
-                >
-                  <AnimatePresence mode="wait">
-                    <motion.div
-                      key={selectedTab}
-                      initial={{ opacity: 0, y: -100 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      transition={{ delay: 0.2, duration: 0.8 }}
-                      exit={{ opacity: 0, y: 100 }}
-                      className="flex items-center h-full justify-end max-600:justify-center max-600:gap-6 max-600:w-screen"
-                    >
-                      {content()}
-                    </motion.div>
-                  </AnimatePresence>
-                </div>
-              </div>
-            </motion.div>
+          {!showContent && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <motion.h1
+                className="text-4xl text-center"
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 100 }}
+                transition={{ duration: 1.5 }}
+              >
+                Welcome To Mike Olie's Portfolio
+              </motion.h1>
+            </div>
           )}
         </AnimatePresence>
+        {showContent && (
+          <motion.div
+            className="relative w-full h-full"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5, duration: 1.5 }}
+          >
+            <picture>
+              <img
+                src={bgGif}
+                alt="Background"
+                className="w-full h-full object-cover absolute"
+                style={{ zIndex: 1 }}
+              />
+            </picture>
+            <div className="flex flex-row max-600:flex-col md:h-screen max-600:h-screen">
+              <div
+                className="relative p-3 w-28"
+                style={{
+                  zIndex: 2,
+                }}
+              >
+                <section className="mb-8 max-600:flex max-600:gap-5 max-600:items-center">
+                  <h1 className="text-3xl">Mike Olie</h1>
+                  <h2 className="text-xl">Software Engineer</h2>
+                </section>
+                <ul>{tabOptions}</ul>
+              </div>
+              <div
+                className="max-h-full relative w-full pr-8 max-600:p-0"
+                style={{
+                  zIndex: 2,
+                }}
+              >
+                <AnimatePresence mode="wait">
+                  <motion.div
+                    key={selectedTab}
+                    initial={{ opacity: 0, y: -100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0.2, duration: 0.8 }}
+                    exit={{ opacity: 0, y: 100 }}
+                    className="flex items-center h-full justify-end max-600:justify-center max-600:gap-6 max-600:w-screen"
+                  >
+                    {content()}
+                  </motion.div>
+                </AnimatePresence>
+              </div>
+            </div>
+          </motion.div>
+        )}
       </main>
     </Layout>
   );
